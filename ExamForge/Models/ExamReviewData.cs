d63d3bb461c3906964fabc5b544b147f271c8576
Bryan May Abulencia <braeanmay@gmail.com>
2026-02-11 08:13:09 +0800

    Fixed the bugs

--- ExamForge/Models/ExamReviewData.cs
+++ ExamForge/Models/ExamReviewData.cs
@@ -16,47 +16,6 @@ public class ExamReviewData
 }
 
 [FirestoreData]
-public class ExamStructure
-{
-    [FirestoreProperty]
-    public string SectionName { get; set; } = "";
-    
-    [FirestoreProperty]
-    public string Description { get; set; } = "";
-    
-    [FirestoreProperty]
-    public int Points { get; set; }
-    
-    [FirestoreProperty]
-    public int QuestionCount { get; set; }
-    
-    [FirestoreProperty]
-    public bool IsComplete { get; set; }
-}
-
-[FirestoreData]
-public class ExamContent
-{
-    [FirestoreProperty]
-    public string QuestionText { get; set; } = "";
-    
-    [FirestoreProperty]
-    public string QuestionType { get; set; } = "";
-    
-    [FirestoreProperty]
-    public List<string> Options { get; set; } = new();
-    
-    [FirestoreProperty]
-    public int Points { get; set; }
-    
-    [FirestoreProperty]
-    public string CorrectAnswer { get; set; } = "";
-    
-    [FirestoreProperty]
-    public bool IsComplete { get; set; }
-}
-
-[FirestoreData]
 public class LoginConfigState
 {
     [FirestoreProperty]
@@ -72,45 +31,7 @@ public class LoginConfigState
     public bool RequireStudentNumber { get; set; }
 }
 
-[FirestoreData]
-public class PublishedExam
-{
-    [FirestoreProperty]
-    public string Id { get; set; } = "";
     
-    [FirestoreProperty]
-    public string Title { get; set; } = "";
-    
-    [FirestoreProperty]
-    public List<ExamStructure> Structures { get; set; } = new();
-    
-    [FirestoreProperty]
-    public List<ExamContent> Contents { get; set; } = new();
-    
-    [FirestoreProperty]
-    public DateTime StartTime { get; set; }
-    
-    [FirestoreProperty]
-    public DateTime EndTime { get; set; }
-    
-    [FirestoreProperty]
-    public int ExamDuration { get; set; }
-    
-    [FirestoreProperty]
-    public DateTime PublishedDate { get; set; }
-    
-    [FirestoreProperty]
-    public string CreatedBy { get; set; } = "";
-    
-    [FirestoreProperty]
-    public string ExamUrl { get; set; } = "";
-    
-    [FirestoreProperty]
-    public string Status { get; set; } = "";
-    
-    [FirestoreProperty]
-    public LoginConfigState? LoginConfig { get; set; }
-}
 
 [FirestoreData]
 public class ExamSubmission
